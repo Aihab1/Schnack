6643b8e16318a1533eb444686ddfe17ffed41fe6
Aihab1 <aihab.umair.che19@itbhu.ac.in>
2020-07-27 18:10:55 +0530

    Change modal design

--- application.py
+++ application.py
@@ -84,7 +84,7 @@ def message(data):
     now = datetime.now()
     today = datetime.now().date()
     time = now.strftime("%H:%M")
-    date = today.strftime('%b, %d %Y')
+    date = today.strftime('%b %d, %Y')
     
     if "allmessages" not in messages[data["namechatroom"]]:
         messages[data["namechatroom"]]["allmessages"] = []
